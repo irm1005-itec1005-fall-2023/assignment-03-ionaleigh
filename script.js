abd5e1b2a115d7e0b6aa8b7296f03c9fa5ae36d2
ionaleigh <ionaleigh228@gmail.com>
2023-11-19 23:51:30 -0500

    final

--- script.js
+++ script.js
@@ -47,6 +47,12 @@
 
 // Initialise an empty array with the variable name todoItems
 
+let todoItems = [];
+let counter = 0;
+
+addToDoItem("Make your bed");
+removeToDoItem(0);
+
 // Function to add a todo to the list
 // It should accept a string as a parameter (text of the todo item)
 // and it should add a new todo item to the todoItems array
@@ -54,10 +60,27 @@
 // It's really important that you have a unique ID for each todo item that you push onto the array
 // the function does not need to return anything
 function addToDoItem(text) {
+//ERROR HANDLING
+  if (!text){
+    console.error("Text cannot be empty.");
+    return;
+  }
+//duplicate IDs
+  if (todoItems.some(item => item.id === counter)){
+    console.error("No duplicate IDs allowed. Please regenerate.");
+    return;
+  }
   // Implement the logic to add a task here
-
-  console.log("NOT YET IMPLEMENTED"); // Remove this line when you start working on the function
+  let todoItem ={
+    id : counter,
+    text : text,
+    completed : false,
+  };
+  todoItems.push(todoItem);
+  counter = counter + 1;
 }
+  //console.log("NOT YET IMPLEMENTED"); // Remove this line when you start working on the function
+
 
 // Function to remove a todo to the list
 // It should accept a number as a parameter (id of the todo item)
@@ -65,10 +88,27 @@ function addToDoItem(text) {
 // that matches the id passed to the function, remove it from the array
 // the function does not need to return anything
 function removeToDoItem(todoId) {
-  // Implement the logic to add a task here
-
-  console.log("NOT YET IMPLEMENTED"); // Remove this line when you start working on the function
+//check that todoId is a valid number
+if (typeof todoId !== 'number' || isNaN(todoId)) {
+  console.error("Invalid input type.");
+  return;
 }
+//check that todoId exists before attempting to remove?
+  let indexToRemove = todoItems.findIndex(item => item.id === todoId);
+  if (indexToRemove === -1){
+    console.error(`Todo item with ID ${todoId} not found.`);
+    return;
+  }
+
+  // Implement the logic to add a task here
+  for (let index = 0; index < todoItems.length; index++){
+    if (todoItems[index].id === todoId){
+      todoItems.splice(index, 1);
+      break;
+    }
+  }
+}
+  //console.log("NOT YET IMPLEMENTED"); // Remove this line when you start working on the function
 
 // Function to mark a task as completed
 // It should accept a number as a parameter (id of the todo item)
@@ -76,10 +116,35 @@ function removeToDoItem(todoId) {
 // that matches the id passed to the function, set its completed property to true
 // the function does not need to return anything
 function markToDoItemAsCompleted(todoId) {
-  // Implement the logic to mark a task as completed here
 
-  console.log("NOT YET IMPLEMENTED"); // Remove this line when you start working on the function
+//check that todoId is a valid number
+if (typeof todoId !== 'number' || isNaN(todoId)) {
+  console.error("Invalid input type.");
+  return;
 }
+  try{
+  /* check todo exists
+  let todoItem = todoItems.find(item => item.id === todoId);
+  if (!todoItem){
+    console.error(`Todo item with ID ${todoId} not found.`);
+    return;
+}*/ //For some reason I cannot get this to work as it always returns the error message saying the item DNE but it does???? any suggestions would be greatly appreciated haha
+
+  // Implement the logic to mark a task as completed here
+  for (let index = 0; index < todoItems.length; index++){
+    if (todoItems[index].id === todoId){
+      todoItems[index].completed = true;
+      break;
+    }
+  }
+} catch (error) {
+  console.error('Error found while marking todo item as completed');
+}
+}
+markToDoItemAsCompleted(1);
+
+
+  //console.log("NOT YET IMPLEMENTED"); // Remove this line when you start working on the function
 
 // Function to delete a task from the array
 // It should accept a number as a parameter (id of the todo item)
@@ -88,19 +153,45 @@ function markToDoItemAsCompleted(todoId) {
 // the function does not need to return anything, though you can return
 // true or false depending on whether the item was successfully deleted
 function deleteToDoItem(todoId) {
-  // Implement the logic to remove a task here
-
-  console.log("NOT YET IMPLEMENTED"); // Remove this line when you start working on the function
+//check that todoId is a valid number
+if (typeof todoId !== 'number' || isNaN(todoId)) {
+  console.error("Invalid input type.");
+  return;
 }
+//check that todoId exists before attempting to delete?
+let indexToRemove = todoItems.findIndex(item => item.id === todoId);
+if (indexToRemove === -1){
+  console.error(`Todo item with ID ${todoId} not found.`);
+  return;
+}
+  // Implement the logic to remove a task here
+  for (let index = 0; index < todoItems.length; index++){
+    if (todoItems[index].id === todoId){
+      todoItems.splice(index, 1);
+      break;
+    }
+  }
+}
+  //console.log("NOT YET IMPLEMENTED"); // Remove this line when you start working on the function
+
 
 // Function to clear all completed tasks
 // Loop through the array of todos, and when you find a todo item that is marked
 // as completed, remove it completely from the array
 function clearCompletedTasks() {
-  // Implement the logic to clear completed tasks here
-
-  console.log("NOT YET IMPLEMENTED"); // Remove this line when you start working on the function
+//check that todoItems is array before clearing?
+if (!Array.isArray(todoItems)) {
+  console.error("Invalid. Please ensure todoItems is an array.");
+  return;
 }
+  // Implement the logic to clear completed tasks here
+  for (let index = 0; index < todoItems.length; index--){ //iterating through the array in reverse order to avoid skipping elements when removing
+    if (todoItems[index].completed === true){
+      todoItems.splice(index, 1);
+    }
+  }
+}
+  //console.log("NOT YET IMPLEMENTED"); // Remove this line when you start working on the function
 
 // You can write your own tests here if you would like to test
 // your code before using the automated tests
